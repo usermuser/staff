5bfd6631614f0b1d480d9a8565dacd70025304d4
usermuser <lwrdrstv@gmail.com>
2018-11-20 16:46:00 +0500

    succesfully passed an list of all arguments from db to template.

--- tornado/handlers.py
+++ tornado/handlers.py
@@ -5,9 +5,13 @@ class AddHandler(tornado.web.RequestHandler):
         self.render('templates/add.html', title='Add')
 
 class MainHandler(tornado.web.RequestHandler):
+    def initilize(self, all_records):
+        print('MainHandler initilized')
+        self.all_records=all_records
+
     def get(self):
         all_records=database.get_all()
-        self.render('templates/list.html', all_records=all_records)
+        self.render('templates/list.html', all_records=all_records, title='Main Handler')
         # TODO PASS ARGUMENTS TO TEMPLATE
 
 class DBInitHandler(tornado.web.RequestHandler):
